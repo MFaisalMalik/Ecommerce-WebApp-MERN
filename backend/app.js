a48f1ad269b85f9b54bc8657e1a76b1ab9c5f04c
Muhammad Faisal Malik <Faisal.malik@astera.com>
2023-01-11 14:08:37 +0500

    Added error handler class

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 
+const errorMidllerware = require("./middleware/error")
+
 app.use(express.json());
 
 // Route Imports
 const product = require("./routes/productRoute");
 
 app.use("/api/v1", product);
+
+// Middleware for Errors
+app.use(errorMidllerware);
 
 module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,5 @@
 const Product = require("../models/productModel");
+const ErrorHandler = require("../utils/errorhandler")
 
 // Creat Product -- Admin
 exports.createProduct = async (req, res, next) => {
@@ -25,10 +26,7 @@ exports.getProductDetails = async (req, res, next) => {
     const product = await Product.findById(req.params.id);
 
     if( !product ){
-        return res.status(500).json({
-            success : false,
-            message : "Product not found"
-        })
+        return next(new ErrorHandler("Product not found", 404))
     }
     
     res.status(200).json({
@@ -42,10 +40,7 @@ exports.updateProduct = async (req, res, next) => {
     let product = await Product.findById(req.params.id);
 
     if( !product ){
-        return res.status(500).json({
-            success : false,
-            message : "Product not found"
-        })
+        return next(new ErrorHandler("Product not found", 404))
     }
 
     product = await Product.findByIdAndUpdate(req.params.id, req.body, {
@@ -66,10 +61,7 @@ exports.deleteProduct = async (req, res, next) => {
     const product = await Product.findById(req.params.id);
 
     if( !product ){
-        return res.status(500).json({
-            success : false,
-            message : "Product not found"
-        })
+        return next(new ErrorHandler("Product not found", 404))
     }
 
     await product.remove();
